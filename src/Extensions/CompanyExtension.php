7322d513d7971aac17ae5ae78e25fb547626ebd3
Martin Soenen <martin.soenen@outlook.fr>
2024-12-11 22:22:06 +0100

    :sparkles: Add company name method

--- src/Extensions/CompanyExtension.php
+++ src/Extensions/CompanyExtension.php
@@ -7,11 +7,29 @@ use Xefi\Faker\Extensions\Extension;
 
 class CompanyExtension extends Extension
 {
+    private array $companies = [
+        "Oakridge Enterprises", "SummitPeak Group", "LibertyWorks Industries",
+        "Pioneer Dynamics", "Northstar Solutions", "Heritage Valley Financial",
+        "BigSky Logistics", "IronTrail Manufacturing", "SilverCrest Energy",
+        "Evergreen Partners", "Redwood Analytics", "Highland Tech Labs",
+        "BlueRiver Medical", "Patriot Systems Inc.", "Stonebridge Holdings",
+        "FreedomForge Corp.", "American Horizon Ventures", "EaglePoint Investments",
+        "Trailblazer Innovations", "Grandview Networks", "Maplewood Industries",
+        "CedarHill Technologies", "LoneStar Utilities", "CapitalEdge Group",
+        "Canyon Ridge Development", "Dakota Plains Energy", "Appalachia Steelworks",
+        "Hudson Bay Logistics", "GreatLakes Solutions", "Phoenix Harbor Corporation"
+    ];
+
     public function ein(): string
     {
         $prefix = $this->randomizer->getBytesFromString(implode(range(0, 9)), 2);
         $suffix = $this->randomizer->getBytesFromString(implode(range(0, 9)), 7);
 
         return $prefix . '-' . $suffix;
+    }
+
+    public function company(): string
+    {
+        return $this->pickArrayRandomElement($this->companies);
     }
 }
--- tests/Unit/CompanyExtensionTest.php
+++ tests/Unit/CompanyExtensionTest.php
@@ -2,11 +2,22 @@
 
 namespace Xefi\Faker\EnUs\Tests\Unit;
 
-use Xefi\Faker\Calculators\Luhn;
+use Random\Randomizer;
 use Xefi\Faker\Container\Container;
+use Xefi\Faker\EnUs\Extensions\CompanyExtension;
 
 final class CompanyExtensionTest extends TestCase
 {
+    protected array $companies = [];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $companyExtension = new CompanyExtension(new Randomizer());
+        $this->companies = (new \ReflectionClass($companyExtension))->getProperty('companies')->getValue($companyExtension);
+    }
+
     public function testEin()
     {
         $faker = new Container(false);
@@ -23,5 +34,18 @@ final class CompanyExtensionTest extends TestCase
             $this->assertEquals(2, strlen($prefix));
             $this->assertEquals(7, strlen($suffix));
         }
+    }
+
+    public function testCompany()
+    {
+        $results = [];
+        for ($i = 0; $i < count($this->companies); $i++) {
+            $results[] = $this->faker->unique()->company();
+        }
+
+        $this->assertEqualsCanonicalizing(
+            $this->companies,
+            $results
+        );
     }
 }
